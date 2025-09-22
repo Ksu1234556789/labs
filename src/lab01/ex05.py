86b935e92629c1ca43342255e1432512349e3d07
Ksenia <ksu_k@MacBook-Pro-Ksusha.local>
2025-09-22 11:45:58 +0300

    задачи

---
+++ src/lab01/ex05.py
@@ -0,0 +1,7 @@
+name = input('ФИО:').strip()
+parts = name.split()
+initials = ''.join(part[0].upper() for part in parts) + '.'
+length =len(' '.join(parts))
+print('Инициалы:', initials)
+print('Длина (символов):', length)
+
